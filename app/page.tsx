cf8e43998830a39752f438e3c7167cdbd6d010b1
Ilyes <ilyeesx@gmail.com>
2026-03-01 16:19:13 +0100

    update liens

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const products = [
     id: 1,
     badge: "/projecteur-lp/badge-1.png",
     name: "NEXGEAR V12 4K",
-    price: "169€",
+    price: "129.99€",
     grade: "A+",
     productImage: "/projecteur-lp/product-nexgear.png",
     scoreImage: "/projecteur-lp/scores-top1.png",
